0a9635e09d637f4127a357e06ad93c34a20e8928
StoDruzey <sergelabada@gmail.com>
2023-07-16 18:57:14 +0200

    kolejny commit

--- app/src/main/java/com/example/sample17l10retrofitcustomlist/UserAdapter.kt
+++ app/src/main/java/com/example/sample17l10retrofitcustomlist/UserAdapter.kt
@@ -1,17 +1,47 @@
 package com.example.sample17l10retrofitcustomlist
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
 import com.example.sample17l10retrofitcustomlist.databinding.ItemUserBinding
+class UserAdapter(context: Context) : ListAdapter<User, UserViewHolder>(DIFF_UTIL) {
 
-class UserAdapter : ListAdapter<User, UserViewHolder>() {
+    private val layoutInflater = LayoutInflater.from(context)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        return UserViewHolder(
+            binding = ItemUserBinding.inflate(layoutInflater, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    companion object {
+        private val DIFF_UTIL = object : DiffUtil.ItemCallback<User>() {
+            override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: User, newItem: User): Boolean {
+                return oldItem.login == newItem.login && oldItem.avatarUrl == newItem.avatarUrl
+            }
+        }
+    }
 }
 
 class UserViewHolder(
     private val binding: ItemUserBinding
 ) : RecyclerView.ViewHolder(binding.root) {
 
-    fun bind(item: User) {
-
+    fun bind(user: User) {
+        with(binding) {
+            imageAvatar.load(user.avatarUrl)
+        }
     }
 }
